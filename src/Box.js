76ec8182fb73d5392fc005eccc85a66cac0480cd
Csilla Bukki <csillabukki@Csillas-MacBook-Air.local>
2024-05-29 18:53:06 -0700

    remove button added

--- src/Box.js
+++ src/Box.js
@@ -1,8 +1,21 @@
-const Box = ({ bgcolor, width = "150px", height = "150px" }) => {
+const Box = ({
+  handleRemove,
+  id,
+  bgcolor,
+  width = "150px",
+  height = "150px",
+}) => {
+  const remove = (e) => {
+    //e.target.parentNode.remove();
+    handleRemove(id);
+  };
   return (
-    <div
-      style={{ backgroundColor: bgcolor, width: width, height: height }}
-    ></div>
+    <div>
+      <div
+        style={{ backgroundColor: bgcolor, width: width, height: height }}
+      ></div>
+      <button onClick={remove}>X</button>
+    </div>
   );
 };
 export default Box;
--- src/BoxList.js
+++ src/BoxList.js
@@ -5,19 +5,19 @@ import "./BoxList.css";
 
 import Box from "./Box";
 const BoxList = () => {
-  const INITIAL_STATE = [
+  /*const INITIAL_STATE = [
     { id: uuid(), bgcolor: "PeachPuff" },
     { id: uuid(), bgcolor: "SkyBlue" },
     { id: uuid(), bgcolor: "MediumPurple" },
-  ];
+  ];*/
   //const [boxes, setBoxes] = useState(INITIAL_STATE);
   const [boxes, setBoxes] = useState([]);
   const addBox = (newBox) => {
-    const updatedBoxes = [...boxes, { ...newBox, id: uuid() }];
-    console.log(updatedBoxes);
     setBoxes((boxes) => [...boxes, { ...newBox, id: uuid() }]);
   };
-
+  const handleRemove = (id) => {
+    setBoxes((boxes) => boxes.filter((box) => box.id !== id));
+  };
   return (
     <div>
       <div className="BoxList-form">
@@ -27,10 +27,12 @@ const BoxList = () => {
       <div className="BoxList-boxes">
         {boxes.map((box) => (
           <Box
+            id={box.id}
             key={box.id}
             bgcolor={box.bgcolor}
             width={box.width}
             height={box.height}
+            handleRemove={handleRemove}
           />
         ))}
       </div>
